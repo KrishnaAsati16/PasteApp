c9e0137d3f3074b2541de5a3a1dc6734abce7840
KrishnaAsati16 <krishnaasati2006@gmail.com>
2026-05-18 15:39:44 +0530

    first commit

---
+++ src/utlis/formatData.js
@@ -0,0 +1,27 @@
+// import {configureStore} from "@reduxjs/toolkit"
+// import pasteReducer from "./pasteSlice"
+
+// export const store = configureStore({
+//   reducer:{
+//       paste: pasteReducer
+//   }
+// })
+export const FormatDate = (date) => {
+  // Ensure the date is a valid Date object
+  const _date = new Date(date);
+
+  // Check if the date is valid
+  if (isNaN(_date)) {
+    console.error('Invalid date');
+    return 'Invalid Date';
+  }
+
+  // Format the date using Intl.DateTimeFormat
+  const formattedDate = new Intl.DateTimeFormat('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  }).format(_date);
+
+  return formattedDate;
+};
